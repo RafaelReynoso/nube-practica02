7ea13e87ad1914ff223597a92bfb5a9aee394a05
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2023-11-01 11:32:19 -0500

    quinto commit

--- practica02/practica02/settings.py
+++ practica02/practica02/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#5&+c2e9wbj5m18&eqesf_u9$0+7gz__&d!mt(&y_o1bnqp1x%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['**']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
